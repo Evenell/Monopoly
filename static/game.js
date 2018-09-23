f08d4c90d58e8fa52ab9362add5944febda46e86
Michael Song <micsong@seas.upenn.edu>
2018-09-22 20:54:37 -0400

    Added some drawing elements

--- static/game.js
+++ static/game.js
@@ -27,11 +27,17 @@ canvas.height = 600;
 var context = canvas.getContext('2d');
 socket.on('state', function(players, bullets) {
 	context.clearRect(0, 0, 800, 600);
-	context.fillStyle = 'green';
+	context.fillStyle = 'black';
 	for (var id in players) {
 		var player = players[id];
 		context.beginPath();
 		context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
 		context.fill();
+		context.moveTo(player.x, player.y);
+		context.arc(player.x, player.y, 15, -1 * Math.PI / 12, 7 * Math.PI / 12)
+		context.lineTo(player.x + 20 * player.unitVectX, player.y + 20 * player.unitVectY);
+		context.closePath();
+		context.stroke();
+
 	}
 });
